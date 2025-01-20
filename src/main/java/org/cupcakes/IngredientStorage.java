23b095ab807e933d31fe3787459cbfe598f386e2
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2025-01-20 09:24:11 +0100

    Update ingredientStorage  add function add and remove

--- src/main/java/org/cupcakes/IngredientStorage.java
+++ src/main/java/org/cupcakes/IngredientStorage.java
@@ -3,6 +3,11 @@ package org.cupcakes;
 import java.util.HashMap;
 
 public class IngredientStorage {
+
+    private HashMap<CupcakeBase, Integer> bases = new HashMap<>();
+    private HashMap<Topping, Integer> toppings = new HashMap<>();
+    private HashMap<Cream, Integer> creams = new HashMap<>();
+
     HashMap<CupcakeBase, Integer> getBases() {
         return new HashMap<>();
     }
@@ -15,21 +20,71 @@ public class IngredientStorage {
         return new HashMap<>();
     }
 
+
+
     void addBase(CupcakeBase base, int quantity) {
+
+        bases.put(base, bases.getOrDefault(base, 0) + quantity);
+
     }
 
     void addTopping(Topping topping, int quantity) {
+
+        toppings.put(topping, toppings.getOrDefault(topping, 0) + quantity);
+
     }
 
     void addCream(Cream cream, int quantity) {
+
+        creams.put(cream, creams.getOrDefault(cream,0) + quantity);
+
     }
 
     void removeBase(CupcakeBase base, int quantity) {
+
+        int currentQuantity = bases.getOrDefault(base,0);
+
+        if (currentQuantity >= quantity){
+            bases.put(base,currentQuantity - quantity);
+        }
+        if (bases.get(base)==0){
+
+            bases.remove(base);
+
+        }
+
     }
 
     void removeTopping(Topping topping, int quantity) {
+
+        int currentQuantity = toppings.getOrDefault(topping,0);
+
+        if(currentQuantity>= quantity){
+
+            toppings.put(topping,currentQuantity -quantity);
+
+        }
+        if (toppings.get(topping)==0){
+            toppings.remove(topping);
+        }
+
+
     }
 
     void removeCream(Cream cream, int quantity) {
+
+        int currentQuantity = creams.getOrDefault(cream, 0);
+
+        if (currentQuantity >= quantity) {
+
+            creams.put(cream, currentQuantity - quantity);
+
+        }
+        if (creams.get(cream) == 0) {
+
+            creams.remove(cream);
+
+        }
+
     }
 }
