bd2900f37b998806e4733d04506da546fc72fe65
Basile Paoli <basile.paoli@gmail.com>
2025-01-20 08:35:32 +0100

    initial commit

---
+++ src/test/java/org/cupcakes/Test.java
@@ -0,0 +1,11 @@
+package org.cupcakes;
+
+
+import static org.junit.Assert.assertEquals;
+
+public class Test {
+    @org.junit.Test
+    public void TestCupcakes() {
+        assertEquals(1, 1);
+    }
+}
