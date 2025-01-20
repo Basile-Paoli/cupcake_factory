02568dfbe14d0ef58c22f7eac7e2bb7325ba34b5
Spatulox <M78stormtrooper@laposte.net>
2025-01-20 09:53:10 +0100

    copy paste for the other remove type

--- src/test/java/org/cupcakes/TestIngredientStorage.java
+++ src/test/java/org/cupcakes/TestIngredientStorage.java
@@ -4,11 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
 import java.util.Map;
-
-import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class TestIngredientStorage {
     private IngredientStorage storage;
@@ -148,29 +144,84 @@ public class TestIngredientStorage {
         Assertions.assertThrows(Exception.class, ()->storage.removeBase(CupcakeBase.VANILLA, 1));
     }
 
-    @Test
-    public void testRemoveBaseExceedStock() {
-        storage.removeBase(CupcakeBase.VANILLA, 1);
-        var hashmapBase = storage.getBases();
-        Assertions.assertFalse(hashmapBase.containsKey(CupcakeBase.VANILLA),
-                "La base vanilla ne devrait plus exister dans le stockage");
-    }
-
     // --------------------------------------------------------------------------- //
 
     @Test
-    public void testRemoveTopping() {
+    public void testRemoveToppingNormal() {
         storage.addTopping(Topping.CHOCOLATECHIPS, 1);
-        Assertions.assertTrue(storage.removeTopping("Chocolate chips"));
-        assertEquals(0, storage.getToppingCount());
+        storage.removeTopping(Topping.CHOCOLATECHIPS, 1);
+        var hashmapTopping = storage.getToppings();
+        Assertions.assertFalse(hashmapTopping.containsKey(Topping.CHOCOLATECHIPS),
+                "Le topping Chocolate Chips ne devrait plus exister dans le stockage");
+    }
+
+    @Test
+    public void testRemoveToppingNormalButStillHaveInDB() {
+        storage.addTopping(Topping.CHOCOLATECHIPS, 10);
+        storage.removeTopping(Topping.CHOCOLATECHIPS, 4);
+
+        Map<Topping, Integer> toppings = storage.getToppings();
+        Assertions.assertTrue(toppings.containsKey(Topping.CHOCOLATECHIPS),
+                "Le topping Chocolate Chips devrait toujours exister dans le stockage");
+        Assertions.assertEquals(6, toppings.get(Topping.CHOCOLATECHIPS),
+                "Il devrait rester seulement 6 Chocolate Chips dans le stockage");
+    }
+
+    @Test
+    public void testRemoveToppingNormalButRemoveMoreThanHaveInDB() {
+        storage.addTopping(Topping.CHOCOLATECHIPS, 2);
+        Assertions.assertThrows(Exception.class, () -> storage.removeTopping(Topping.CHOCOLATECHIPS, 5));
+    }
+
+    @Test
+    public void testRemoveToppingNegative() {
+        storage.addTopping(Topping.CHOCOLATECHIPS, 1);
+        Assertions.assertThrows(Exception.class, () -> storage.removeTopping(Topping.CHOCOLATECHIPS, -1));
+    }
+
+    @Test
+    public void testRemoveToppingDontExist() {
+        Assertions.assertThrows(Exception.class, () -> storage.removeTopping(Topping.CHOCOLATECHIPS, 1));
     }
 
     // --------------------------------------------------------------------------- //
 
     @Test
-    public void testRemoveCream() {
-        storage.addCream(Cream.VANILLA, 1);
-        Assertions.assertTrue(storage.removeCream("Buttercream"));
-        assertEquals(0, storage.getCreamCount());
+    public void testRemoveCreamNormal() {
+        storage.addCream(Cream.RASPBERRY, 1);
+        storage.removeCream(Cream.RASPBERRY, 1);
+        var hashmapCream = storage.getCreams();
+        Assertions.assertFalse(hashmapCream.containsKey(Cream.RASPBERRY),
+                "La crème Buttercream ne devrait plus exister dans le stockage");
     }
+
+    @Test
+    public void testRemoveCreamNormalButStillHaveInDB() {
+        storage.addCream(Cream.RASPBERRY, 8);
+        storage.removeCream(Cream.RASPBERRY, 3);
+
+        Map<Cream, Integer> creams = storage.getCreams();
+        Assertions.assertTrue(creams.containsKey(Cream.RASPBERRY),
+                "La crème Buttercream devrait toujours exister dans le stockage");
+        Assertions.assertEquals(5, creams.get(Cream.RASPBERRY),
+                "Il devrait rester seulement 5 Buttercream dans le stockage");
+    }
+
+    @Test
+    public void testRemoveCreamNormalButRemoveMoreThanHaveInDB() {
+        storage.addCream(Cream.RASPBERRY, 2);
+        Assertions.assertThrows(Exception.class, () -> storage.removeCream(Cream.RASPBERRY, 5));
+    }
+
+    @Test
+    public void testRemoveCreamNegative() {
+        storage.addCream(Cream.RASPBERRY, 1);
+        Assertions.assertThrows(Exception.class, () -> storage.removeCream(Cream.RASPBERRY, -1));
+    }
+
+    @Test
+    public void testRemoveCreamDontExist() {
+        Assertions.assertThrows(Exception.class, () -> storage.removeCream(Cream.RASPBERRY, 1));
+    }
+
 }
